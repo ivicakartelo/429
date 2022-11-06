eb3f086df48184573e7c92b5c23b66604e208db5
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-11-06 22:13:30 +0100

    TimeAgo from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsList } from './features/posts/PostsList'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/">Posts</Link>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
+            )}
+          />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Redirect to="/" />
+        </Switch>
+    </Router>    
   );
 }
-
 export default App;
